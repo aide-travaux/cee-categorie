b3ed4fe24c5113a63ebec72ec925c3303b9c19db
Adrien Rosi <adrienrosi@gmail.com>
2020-02-06 19:52:50 +0100

    Update Entries

--- Data/Entries.php
+++ Data/Entries.php
@@ -2,22 +2,8 @@
 
 namespace AideTravaux\CEE\Categorie\Data;
 
-abstract class Entries
-{
-    /**
-     * @property array
-     */
-    const CODES_REGION = [
-        '01', '02', '03', '04', '06', '11', '24', '27', '28',
-        '32', '44', '52', '53', '75', '76', '84', '93', '94'
-    ];
+use AideTravaux\Core\Entries as CoreEntries;
 
-    /**
-     * @property array
-     */
-    const CATEGORIES_CEE = [
-        'categorie_cee_1' => 'Classique',
-        'categorie_cee_2' => 'Précarité énergétique',
-        'categorie_cee_3' => 'Grande précarité énergétique'
-    ];
+abstract class Entries extends CoreEntries
+{
 }
