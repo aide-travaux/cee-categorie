9b6853cd06cc7ce1f1b38d480f4df8b921641594
Adrien Rosi <adrienrosi@gmail.com>
2020-02-06 19:54:58 +0100

    Add tests

--- Model/DataInterface.php
+++ Model/DataInterface.php
@@ -4,27 +4,12 @@ namespace AideTravaux\CEE\Categorie\Model;
 
 interface DataInterface
 {
-    /**
-     * Retourne le code administratif de la région
-     * @return string
-     */
     public function getCodeRegion(): string;
 
-    /**
-     * Retourne le code administratif du département
-     * @return string
-     */
     public function getCodeDepartement(): string;
 
-    /**
-     * Retourne le nombre de personnes composant le ménages
-     * @return int
-     */
     public function getCompositionMenage(): int;
 
-    /**
-     * Retourne les ressources du ménage
-     */
     public function getRessourcesMenage(): float;
 
 }
